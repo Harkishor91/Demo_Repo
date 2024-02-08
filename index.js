353a1dbb24577ce1484a398e25591aa850421dc3
Harkishor <harkishor5622@gmail.com>
2024-02-08 21:50:13 +0530

    Basic Node JS project

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log('Hello this is my JS file')
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT;
+const dbUrl = process.env.DbUrl;
+
+// Middleware to parse incoming request bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("THis is code is working on localhost");
+});
+
+const startServer = async () => {
+    try {
+        await mongoose.connect(dbUrl);
+        console.log("Connected to MongoDB");
+        app.listen(port, () => {
+          console.log(`Server running on port ${port}`);
+        });
+      } catch (err) {
+        console.error("Error connecting to MongoDB:", err);
+        process.exit(1);
+      }
+};
+
+startServer();
